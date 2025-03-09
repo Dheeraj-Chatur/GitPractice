9786677de0f5eb8a1a077a1317f405dcb8ad65d3
Dheerj Chatur <dhiraj.chatur@algodomain.com>
2025-03-09 11:54:45 +0530

    commented footer para

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className="border p-2 bg-amber-200" >
             <div className="flex justify-center space-x-20">
             <p>Footer</p>
-            <p>@All right reserved</p>
+            {/* <p>@All right reserved</p> */}
             </div>
         </footer>
     )
